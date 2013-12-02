f03d7ec31dda8711ad154ee3f80bd85893f3c3c3
Kevin Whelan <km.whelan93@gmail.com>
2013-12-02 18:34:18 -0500

    modified categories.php to include image url

--- views/categories.php
+++ views/categories.php
@@ -2,17 +2,17 @@
 	/*
 	 * Given a category name, gets all activities in that category alphabetically
 	*/
-	include_once 'dblogin.php';
+	include_once 'dbloginkevin.php';
 	$results = array();
-	if ($stmt->prepare('SELECT activity_name
+	if ($stmt->prepare('SELECT activity_name, activity_image_url
 						FROM Activities
 						WHERE category=?
 						ORDER BY activity_name ASC')) {
 		$stmt->bind_param('s', $category_name);
 		$stmt->execute();
-		$stmt->bind_result($activity_name);
+		$stmt->bind_result($activity_name, $activity_image_url);
 		while ($stmt->fetch()) {
-			$results[] = $activity_name;
+			$results[] = array("activity_name" => $activity_name, "activity_image_url" => $activity_image_url);
 		}
 
 
