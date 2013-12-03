4d0f9acf4adb5cba4a9c523408bd3af69f41a0df
Kevin Whelan <km.whelan93@gmail.com>
2013-12-02 20:19:04 -0500

    added locations to get group

--- views/get_group.php
+++ views/get_group.php
@@ -6,14 +6,15 @@
 	include_once 'dbloginkevin.php';
 
 	$group = array();
-	if ($stmt->prepare('SELECT activity_name, building_name, start_time, end_time, recurrence
+	if ($stmt->prepare('SELECT activity_name, building_name, start_time, end_time, recurrence, building_x, building_y
 						FROM Groups
+						NATURAL JOIN Buildings
 						WHERE group_id=?')) {
 		$stmt->bind_param('s', $group_id);
 		$stmt->execute();
-		$stmt->bind_result($activity_name, $building_name, $start_time, $end_time, $recurrence);
+		$stmt->bind_result($activity_name, $building_name, $start_time, $end_time, $recurrence, $building_x, $building_y);
 		if ($stmt->fetch()) {
-			$group = array("activity_name" => $activity_name, "building_name" => $building_name, "start_time" => $start_time, "end_time" => $end_time, "recurrence" => $recurrence);
+			$group = array("activity_name" => $activity_name, "building" => array("building_name" => $building_name, "building_x" => $building_x, "building_y" => $building_y), "start_time" => $start_time, "end_time" => $end_time, "recurrence" => $recurrence);
 		}
 		
 	}
