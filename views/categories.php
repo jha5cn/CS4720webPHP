0d91a79cebb0f67e284ab63722ae5b05d8f57b7e
Kevin Whelan <km.whelan93@gmail.com>
2013-12-02 21:01:39 -0500

    fixed categories

--- views/categories.php
+++ views/categories.php
@@ -4,15 +4,15 @@
 	*/
 	include_once 'dbloginkevin.php';
 	$results = array();
-	if ($stmt->prepare('SELECT activity_name, activity_image_url
+	if ($stmt->prepare('SELECT activity_name
 						FROM Activities
 						WHERE category=?
 						ORDER BY activity_name ASC')) {
 		$stmt->bind_param('s', $category_name);
 		$stmt->execute();
-		$stmt->bind_result($activity_name, $activity_image_url);
+		$stmt->bind_result($activity_name);
 		while ($stmt->fetch()) {
-			$results[] = array("activity_name" => $activity_name, "activity_image_url" => $activity_image_url);
+			$results[] = $activity_name;
 		}
 
 
